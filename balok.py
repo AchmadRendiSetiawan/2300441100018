00d4ad812dfaa8de922aa053e200fef016bea02e
AchmadRendiSetiawan <143859472+AchmadRendiSetiawan@users.noreply.github.com>
2023-09-07 22:02:32 +0700

    Add files via upload
    nama: achmad rendi setiawan
    Nim : 230441100018

---
+++ balok.py
@@ -0,0 +1,5 @@
+panjang = int(input("masukkan nilai panjang =  ")) 
+lebar = int(input("masukkan nilai lebar = "))
+tinggi = int(input("masukkan nilai tinggi = "))
+hasil = panjang * lebar * tinggi
+print("nilai dari persegi adalah = ", hasil)
---
+++ tabung.py
@@ -0,0 +1,4 @@
+jari = int(input("masukkan nilai jari = "))
+tinggi = int(input("masukkan nilai tinggi = "))
+hasil = 22/7*jari*tinggi
+print("nilai volume tabung adalah = ", hasil)
